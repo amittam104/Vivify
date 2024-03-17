7ee56d528ce0320f9382d7b41543fbb3fa69cb37
Amit Tambulkar <amittambulkar104@gmail.com>
2024-03-17 16:04:20 +0530

    Add BMI calculation logic

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
 "use strict";
 
 // Variables
+const radioImperial = document.getElementById("imperial");
+const radioMetric = document.getElementById("metric");
+
 const inputWeight = document.getElementById("weight");
 const inputHeight = document.getElementById("height");
+
+const labelWelcome = document.getElementById("display-welcome");
+const labelBMI = document.getElementById("display-bmi");
+const labelBMIValue = document.getElementById("bmi-value");
+const labelBMIText = document.getElementById("bmi-text");
+
 const btnSubmit = document.getElementById("submit");
 
 let bmiValue;
+
+// Unit Conversion
+radioImperial.addEventListener("click", function (e) {
+  e.preventDefault;
+  inputWeight.placeholder = "Pound";
+  inputHeight.placeholder = "inch";
+});
+
+radioMetric.addEventListener("click", function (e) {
+  e.preventDefault;
+  inputWeight.placeholder = "Kg";
+  inputHeight.placeholder = "cm";
+});
+// BMI Calculation and Display
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -15,4 +38,25 @@ btnSubmit.addEventListener("click", function (e) {
     Number(inputWeight.value) /
     (heightInMeters * heightInMeters)
   ).toFixed(1);
+
+  labelBMIValue.textContent = bmiValue;
+
+  if (bmiValue < 18.5) {
+    labelBMIText.textContent = `Your BMI suggests you you are Underweight. See below for
+    more information.`;
+  } else if (bmiValue > 18.5 && bmiValue < 24.9) {
+    labelBMIText.textContent = `Your BMI suggests you weight is in healthy range. See below for
+    more information.`;
+  } else if (bmiValue > 25 && bmiValue < 29.9) {
+    labelBMIText.textContent = `Your BMI suggests you are in overweight category. See below for
+    more information.`;
+  } else {
+    labelBMIText.textContent = `Your BMI suggests you are obese. See below for
+    more information.`;
+  }
+  labelWelcome.classList.add("hidden");
+
+  labelBMI.classList.remove("hidden");
+
+  labelBMI.classList.add("flex");
 });
