385a8897215ea5c8bfa3179f5b2b185d48f81b0f
Amit Tambulkar <amittambulkar104@gmail.com>
2024-03-23 17:23:18 +0530

    chore: add functionality and validation logic

--- src/main.js
+++ src/main.js
@@ -11,48 +11,55 @@ const labelWelcome = document.getElementById("display-welcome");
 const labelBMI = document.getElementById("display-bmi");
 const labelBMIValue = document.getElementById("bmi-value");
 const labelBMIText = document.getElementById("bmi-text");
+const labelSubmitError = document.getElementById("error");
 
 const btnSubmit = document.getElementById("submit");
 
 let bmiValue;
 
 const calcDisplayBMI = function () {
-  if (radioMetric.checked) {
-    inputHeightValue = Number(inputHeight.value) / 100;
-    bmiValue = (
-      Number(inputWeight.value) /
-      (inputHeightValue * inputHeightValue)
-    ).toFixed(1);
-  }
-
-  if (radioImperial.checked) {
-    inputHeightValue = Number(inputHeight.value);
-    bmiValue = (
-      (Number(inputWeight.value) / (inputHeightValue * inputHeightValue)) *
-      703
-    ).toFixed(1);
-  }
-
-  labelBMIValue.textContent = bmiValue;
-
-  if (bmiValue < 18.5) {
-    labelBMIText.textContent = `Your BMI suggests you you are Underweight. See below for
-    more information.`;
-  } else if (bmiValue > 18.5 && bmiValue < 24.9) {
-    labelBMIText.textContent = `Your BMI suggests you weight is in healthy range. See below for
-    more information.`;
-  } else if (bmiValue > 25 && bmiValue < 29.9) {
-    labelBMIText.textContent = `Your BMI suggests you are in overweight category. See below for
-    more information.`;
+  if (!inputHeight.value || !inputWeight.value) {
+    labelSubmitError.classList.remove("hidden");
   } else {
-    labelBMIText.textContent = `Your BMI suggests you are obese. See below for
+    if (radioMetric.checked) {
+      inputHeightValue = Number(inputHeight.value) / 100;
+      bmiValue = (
+        Number(inputWeight.value) /
+        (inputHeightValue * inputHeightValue)
+      ).toFixed(1);
+    }
+
+    if (radioImperial.checked) {
+      inputHeightValue = Number(inputHeight.value);
+      bmiValue = (
+        (Number(inputWeight.value) / (inputHeightValue * inputHeightValue)) *
+        703
+      ).toFixed(1);
+    }
+
+    labelBMIValue.textContent = bmiValue;
+
+    if (bmiValue < 18.5) {
+      labelBMIText.textContent = `Your BMI suggests you you are Underweight. See below for
     more information.`;
+    } else if (bmiValue > 18.5 && bmiValue < 24.9) {
+      labelBMIText.textContent = `Your BMI suggests you weight is in healthy range. See below for
+    more information.`;
+    } else if (bmiValue > 25 && bmiValue < 29.9) {
+      labelBMIText.textContent = `Your BMI suggests you are in overweight category. See below for
+    more information.`;
+    } else {
+      labelBMIText.textContent = `Your BMI suggests you are obese. See below for
+    more information.`;
+    }
+    labelWelcome.classList.add("hidden");
+
+    labelBMI.classList.remove("hidden");
+
+    labelBMI.classList.add("flex");
   }
-  labelWelcome.classList.add("hidden");
 
-  labelBMI.classList.remove("hidden");
-
-  labelBMI.classList.add("flex");
+  inputWeight.value = inputHeight.value = "";
 };
 
 let inputHeightValue;
