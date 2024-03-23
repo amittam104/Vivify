654618604fd016639ef78a26a670fb55dc2c95fb
Amit Tambulkar <amittambulkar104@gmail.com>
2024-03-23 10:57:31 +0530

    chore: add form initial logic

--- src/bmi.js
+++ src/bmi.js
@@ -20,3 +20,25 @@ radioMetric.addEventListener("click", function () {
   inputWeight.placeholder = "kg";
   inputHeight.placeholder = "cm";
 });
+
+let bmr;
+let maintenanceCalories;
+
+btnSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (inputAge && inputGender && inputWeight && inputHeight && inputActivity) {
+    // Check if Metric unit
+    if (inputWeight.placeholder === "kg" && inputHeight.placeholder === "cm") {
+      bmr =
+        10 * inputWeight.value +
+        6.25 * inputHeight.value -
+        5 * inputAge.value +
+        5;
+
+      maintenanceCalories = Math.floor(bmr * inputActivity.value);
+    }
+  }
+
+  console.log(`${maintenanceCalories} Kcal/day`);
+});
