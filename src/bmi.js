8c7af0d3467557a7a0246221f733b4dacf55b97f
Amit Tambulkar <amittambulkar104@gmail.com>
2024-03-23 10:34:25 +0530

    chore: variables declaration

--- src/bmi.js
+++ src/bmi.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const radioMetric = document.getElementById("metric");
+const radioImperial = document.getElementById("imperial");
+
+const inputAge = document.getElementById("age");
+const inputGender = document.getElementById("gender");
+const inputWeight = document.getElementById("weight");
+const inputHeight = document.getElementById("height");
+const inputActivity = document.getElementById("activity");
+
+const btnSubmit = document.getElementById("submit");
+
+radioImperial.addEventListener("click", function () {
+  inputWeight.placeholder = "lb";
+  inputHeight.placeholder = "in";
+});
+
+radioMetric.addEventListener("click", function () {
+  inputWeight.placeholder = "kg";
+  inputHeight.placeholder = "cm";
+});
